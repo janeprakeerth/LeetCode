44e0129107319b6286347d4f6a8bdec65491693f
janeprakeerth <92048460+janeprakeerth@users.noreply.github.com>
2023-01-29 19:19:07 +0530

    Time: 3 ms (56.22%), Space: 5.8 MB (96.75%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
-        if(n==-2147483648){
-            return false;
-        }
-        int a = n-1;
-        int b = n&a;
-        cout<<b;
-       
-        if(b==0){
-            return true;
-        }else{
-            return false;
-        }
+        if(n<=0) return false;
+        return ((n&(n-1))==0);
     }
 };
