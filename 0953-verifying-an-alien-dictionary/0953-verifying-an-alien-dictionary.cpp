43a93a8953d0c12e88306ee5ce0bdb64910be0be
janeprakeerth <92048460+janeprakeerth@users.noreply.github.com>
2023-02-03 00:03:50 +0530

    Time: 3 ms (93.87%), Space: 9.8 MB (25.79%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -5,25 +5,25 @@ public:
         for(int i=0;i<order.length();i++){
             m[order[i]] = i+1;
         }
-        for(int i=0;i<words.size();i++){
-            for(int j=i+1;j<words.size();j++){
+        for(int i=0;i<words.size()-1;i++){
+
                 int s1 = words[i].length();
-                int s2 = words[j].length();
+                int s2 = words[i+1].length();
                 int num = min(s1,s2);
                 bool x = true;
                 for(int k=0;k<num;k++){
-                    if(m[words[i][k]]<m[words[j][k]]){
+                    if(m[words[i][k]]<m[words[i+1][k]]){
                         x = false;
                         break;
                     }
-                    if(m[words[i][k]]>m[words[j][k]]){
+                    if(m[words[i][k]]>m[words[i+1][k]]){
                         return false;
                     }
                 }
                 if(s1>s2 &&x){
                     return false;
                 }
-            }
+            
         }
         return true;
     }
